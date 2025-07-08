aee731c956a9e16abe60f15c9b47fc0b32a3b0b1
silent47hm <maazm7391@gmail.com>
2025-07-08 11:27:40 +0530

    25% completed commit

---
+++ 01basicsofreactvite/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from "./youtube"
+
+function App() {
+
+  return (
+    <>
+   <h1>Vite react App</h1>
+   <h1>Vite react App</h1>
+   <h1>Vite react App</h1>
+   <Youtube/>
+    </>
+  )
+}
+
+export default App
---
+++ readme.md
@@ -0,0 +1,1 @@
+#start of react section 23
